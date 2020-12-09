7b311e72acc76e5d497d6acccd27fe0460cf63f2
nicodes <nico@ni.codes>
2020-12-08 17:39:38 -0700

    last minute changes, tooltips for methodologies

--- ui/src/components/about/About.js
+++ ui/src/components/about/About.js
@@ -8,7 +8,6 @@ const About = () => {
         <div style={{ maxWidth: '60%', marginLeft: 'auto', marginRight: 'auto' }}>
             <h1>About</h1>
             <p>This Contact Time Calculator calculates the amount of contact time needed to provide adequate inactivation of Giardia and viruses for a range of disinfectants. These calculations can be useful for determining compliance with the Surface Water Treatment Rule.</p>
-            <p>The contact time provided is calculated based on user inputs of the disinfectant residual and the time provided.</p>
             <p>The amount contact time needed is determined from user inputs of the type of disinfectant, the logs of inactivation needed, temperature, pH, and disinfectant residual. The calculator uses the methods and tables in EPA Manual EPA815-R-99-013, titled “Disinfection Profiling and Benchmarking Guidance Manual”, dated August 1999, for determining the amount of inactivation needed.</p>
             <p><a href={pdf} download>Click here to download EPA source documents</a></p>
             <p>This project is open source and can be viewed on <a href='http://github.com/nicodes/ctcalc'>github</a>.</p>
