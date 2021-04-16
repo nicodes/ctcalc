19c213d86c186f0644372122fc8f9e461b961904
nicodes <nico@ni.codes>
2021-04-16 13:27:13 -0600

    remove donate button

--- ui/src/components/app/App.js
+++ ui/src/components/app/App.js
@@ -1,27 +1,30 @@
-import React from 'react';
-import {
-  Switch,
-  Route,
-  Link,
-  useLocation
-} from "react-router-dom";
+import React from "react";
+import { Switch, Route, Link, useLocation } from "react-router-dom";
 
-import Calculator from '../calculator/Calculator'
-import About from '../about/About'
-import githubSvg from '../../assets/github.svg'
-import { style, active } from './app.module.scss'
+import Calculator from "../calculator/Calculator";
+import About from "../about/About";
+import githubSvg from "../../assets/github.svg";
+import { style, active } from "./app.module.scss";
 
 const App = () => {
-  const { pathname } = useLocation()
+  const { pathname } = useLocation();
   return (
     <div id="app" className={style}>
       <nav>
-        <Link className={pathname === '/' ? active : null} to="/">CT Calc</Link>
-        <Link className={pathname === '/about' ? active : null} to="/about">About</Link>
+        <Link className={pathname === "/" ? active : null} to="/">
+          CT Calc
+        </Link>
+        <Link
+          to="/about"
+          className={pathname === "/about" ? active : null}
+          style={{ marginLeft: "auto" }}
+        >
+          About
+        </Link>
         <div></div>
-        <a style={{ marginLeft: 'auto' }} href='https://www.paypal.com/donate?hosted_button_id=TSJZP6V8WXKSN'>Donate</a>
-        <a style={{ width: '25px' }} href='https://github.com/nicodes/ctcalc'>
-          <img style={{ width: '100%' }} src={githubSvg} alt='github logo' />
+        {/* <a style={{ marginLeft: 'auto' }} href='https://www.paypal.com/donate?hosted_button_id=TSJZP6V8WXKSN'>Donate</a> */}
+        <a style={{ width: "25px" }} href="https://github.com/nicodes/ctcalc">
+          <img style={{ width: "100%" }} src={githubSvg} alt="github logo" />
         </a>
       </nav>
       <main>
@@ -34,8 +37,8 @@ const App = () => {
           </Route>
         </Switch>
       </main>
-    </div >
+    </div>
   );
-}
+};
 
 export default App;
