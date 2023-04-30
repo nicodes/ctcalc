f5a352da6ae5a6507317dd936521f2bdbd38af2c
Nico Zessoules <35477151+nicodes@users.noreply.github.com>
2023-04-30 00:29:58 -0600

    Fix vercel adapter (#13)
    * fix vercel adapter
    
    * try with only vercel adapter
    
    * fix local build adapter
    
    * add preview:local script
    
    * remove deno
    
    * update name and version
    
    * add vercel env var

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from "astro/config";
 import node from "@astrojs/node";
+import vercelServerless from "@astrojs/vercel/serverless";
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: !!process.env.VERCEL
+    ? vercelServerless()
+    : node({
+        mode: "standalone",
+      }),
   integrations: [svelte()],
 });
