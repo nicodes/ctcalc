22b75aa3ae1c2f2401ebb0f31419d32d7df6f3dc
nicodes <nicozessoules@gmail.com>
2023-04-29 19:19:59 -0600

    update API to return JSON

--- web/src/pages/api/calc.ts
+++ web/src/pages/api/calc.ts
@@ -1,9 +1,4 @@
-import { fcFormula } from "../../utils/formula";
-import { getInactivationFromJSON } from "../../utils/inactivation";
-import {
-  linearInterpolate,
-  trilinearInterpolate,
-} from "../../utils/interpolate";
+import { getInactivation } from "../../utils/inactivation";
 import { parseQueryParams } from "../../utils/parseQueryParams";
 import { validate } from "../../utils/validate";
 
@@ -11,107 +6,15 @@ export async function get({ params, request }) {
   const queryParams = parseQueryParams(request.url);
   const [validatedParams, error] = validate(queryParams);
 
-  if (!validatedParams) return { body: error }; // TODO put error code
+  if (error)
+    return new Response(JSON.stringify({ error }), {
+      status: 500,
+      headers: { "Content-Type": "application/json" },
+    });
 
-  const {
-    methodology,
-    disinfectant,
-    pathogen,
-    temperature,
-    ph,
-    concentration,
-    inactivationLog,
-  } = validatedParams;
-
-  // boolean helpers
-  const isFormula = methodology === "formula";
-  const isFreeChlorine = disinfectant === "free_chlorine";
-  const isGiardia = pathogen === "giardia";
-
-  // temperature helpers
-  const [temperatureMin, n] = isFreeChlorine ? [0.5, 5] : [1.0, 1];
-  const temperatureSanitized =
-    temperature < temperatureMin ? temperatureMin : temperature;
-  const t = temperatureSanitized / n;
-  const temperatureLow = Math.max(temperatureMin, Math.floor(t) * n);
-  const temperatureHigh = Number(Math.ceil(t) * n);
-
-  // Formula
-  if (isFormula) {
-    const result = fcFormula(inactivationLog, temperature, concentration, ph);
-    return { body: `${result}` };
-  }
-
-  // TODO what to do about rounding?
-
-  // Free Chlorine and Giardia requires trilinear interpolation over temperature, ph, and concentration
-  console.log("test", isFreeChlorine, isGiardia);
-  if (isFreeChlorine && isGiardia) {
-    const phLow = Number(Math.floor(ph / 0.5) * 0.5);
-    const phHigh = Number(Math.ceil(ph / 0.5) * 0.5);
-
-    const concentrationLow = Number(Math.floor(concentration / 0.2) * 0.2);
-    const concentrationHigh = Number(Math.ceil(concentration / 0.2) * 0.2);
-
-    const inactivations = [
-      [temperatureLow, phLow, concentrationLow],
-      [temperatureLow, phLow, concentrationHigh],
-      [temperatureLow, phHigh, concentrationLow],
-      [temperatureLow, phHigh, concentrationHigh],
-      [temperatureHigh, phLow, concentrationLow],
-      [temperatureHigh, phLow, concentrationHigh],
-      [temperatureHigh, phHigh, concentrationLow],
-      [temperatureHigh, phHigh, concentrationHigh],
-    ].map(([t, p, c]) =>
-      getInactivationFromJSON(disinfectant, pathogen, t, c, p, inactivationLog)
-    );
-
-    const interpolatedInactivation = trilinearInterpolate(
-      temperature,
-      temperatureLow,
-      temperatureHigh,
-      ph,
-      phLow,
-      phHigh,
-      concentration,
-      concentrationLow,
-      concentrationHigh,
-      inactivations[0],
-      inactivations[1],
-      inactivations[2],
-      inactivations[3],
-      inactivations[4],
-      inactivations[5],
-      inactivations[6],
-      inactivations[7]
-    );
-
-    return {
-      body: `${interpolatedInactivation}`,
-    };
-  }
-
-  console.log("Linear interpolation over temperature");
-  const inactivations = [temperatureLow, temperatureHigh].map((t) =>
-    getInactivationFromJSON(
-      disinfectant,
-      pathogen,
-      t,
-      undefined,
-      undefined,
-      inactivationLog
-    )
-  );
-
-  const interpolatedInactivation = linearInterpolate(
-    temperature,
-    temperatureLow,
-    temperatureHigh,
-    inactivations[0],
-    inactivations[1]
-  );
-
-  return {
-    body: `${interpolatedInactivation}`,
-  };
+  const inactivation = getInactivation(validatedParams);
+  return new Response(JSON.stringify({ inactivation }), {
+    status: 200,
+    headers: { "Content-Type": "application/json" },
+  });
 }
--- web/src/utils/inactivation.ts
+++ web/src/utils/inactivation.ts
@@ -1,4 +1,6 @@
 import inactivations from "../data/inactivations.json";
+import { fcFormula } from "./formula";
+import { trilinearInterpolate, linearInterpolate } from "./interpolate";
 import type { Disinfectant, Pathogen } from "./validate";
 
 /**
@@ -20,15 +22,6 @@ export function getInactivationFromJSON(
   ph: number | undefined,
   inactivationLog: number
 ) {
-  console.log(
-    "lookup:",
-    disinfectant,
-    pathogen,
-    temperature,
-    concentration,
-    ph,
-    inactivationLog
-  );
   if (disinfectant === "free_chlorine" && pathogen === "giardia")
     return (inactivations as any)[disinfectant][pathogen][
       temperature.toFixed(1)
@@ -37,4 +30,116 @@ export function getInactivationFromJSON(
   return (inactivations as any)[disinfectant][pathogen][temperature.toFixed(1)][
     inactivationLog.toFixed(1)
   ];
+}
+
+/**
+ * Calculates the inactivation value based on the provided validated parameters.
+ * @param validatedParams Object containing validated parameters used in the calculation:
+ * @param validatedParams.methodology Methodology used in the calculation. Can be "formula", "experimental", or "extrapolated".
+ * @param validatedParams.disinfectant Type of disinfectant used in the calculation. Can be "free_chlorine" or "monochloramine".
+ * @param validatedParams.pathogen Type of pathogen used in the calculation. Can be "giardia" or "cryptosporidium".
+ * @param validatedParams.temperature Temperature value in degrees Celsius.
+ * @param validatedParams.ph pH value.
+ * @param validatedParams.concentration Concentration value in mg/L.
+ * @param validatedParams.inactivationLog Inactivation log value.
+ * @returns Calculated inactivation value as a number.
+ */
+export function getInactivation(validatedParams: any): number {
+  const {
+    methodology,
+    disinfectant,
+    pathogen,
+    temperature,
+    ph,
+    concentration,
+    inactivationLog,
+  } = validatedParams;
+
+  // boolean helpers
+  const isFormula = methodology === "formula";
+  const isFreeChlorine = disinfectant === "free_chlorine";
+  const isGiardia = pathogen === "giardia";
+
+  // temperature helpers
+  const [temperatureMin, n] = isFreeChlorine ? [0.5, 5] : [1.0, 1];
+  const temperatureSanitized =
+    temperature < temperatureMin ? temperatureMin : temperature;
+  const t = temperatureSanitized / n;
+  const temperatureLow = Math.max(temperatureMin, Math.floor(t) * n);
+  const temperatureHigh = Number(Math.ceil(t) * n);
+
+  // Formula
+  if (isFormula) {
+    const result = fcFormula(inactivationLog, temperature, concentration, ph);
+    return result;
+  }
+
+  // TODO what to do about rounding?
+
+  // Free Chlorine and Giardia requires trilinear interpolation over temperature, ph, and concentration
+  console.log("test", isFreeChlorine, isGiardia);
+  if (isFreeChlorine && isGiardia) {
+    const phLow = Number(Math.floor(ph / 0.5) * 0.5);
+    const phHigh = Number(Math.ceil(ph / 0.5) * 0.5);
+
+    const concentrationLow = Number(Math.floor(concentration / 0.2) * 0.2);
+    const concentrationHigh = Number(Math.ceil(concentration / 0.2) * 0.2);
+
+    const inactivations = [
+      [temperatureLow, phLow, concentrationLow],
+      [temperatureLow, phLow, concentrationHigh],
+      [temperatureLow, phHigh, concentrationLow],
+      [temperatureLow, phHigh, concentrationHigh],
+      [temperatureHigh, phLow, concentrationLow],
+      [temperatureHigh, phLow, concentrationHigh],
+      [temperatureHigh, phHigh, concentrationLow],
+      [temperatureHigh, phHigh, concentrationHigh],
+    ].map(([t, p, c]) =>
+      getInactivationFromJSON(disinfectant, pathogen, t, c, p, inactivationLog)
+    );
+
+    const interpolatedInactivation = trilinearInterpolate(
+      temperature,
+      temperatureLow,
+      temperatureHigh,
+      ph,
+      phLow,
+      phHigh,
+      concentration,
+      concentrationLow,
+      concentrationHigh,
+      inactivations[0],
+      inactivations[1],
+      inactivations[2],
+      inactivations[3],
+      inactivations[4],
+      inactivations[5],
+      inactivations[6],
+      inactivations[7]
+    );
+
+    return interpolatedInactivation;
+  }
+
+  console.log("Linear interpolation over temperature");
+  const inactivations = [temperatureLow, temperatureHigh].map((t) =>
+    getInactivationFromJSON(
+      disinfectant,
+      pathogen,
+      t,
+      undefined,
+      undefined,
+      inactivationLog
+    )
+  );
+
+  const interpolatedInactivation = linearInterpolate(
+    temperature,
+    temperatureLow,
+    temperatureHigh,
+    inactivations[0],
+    inactivations[1]
+  );
+
+  return interpolatedInactivation;
 }
