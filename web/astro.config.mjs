9dd81bf111e541915667e3f2dfeda69247fb2c9f
Nico Zessoules <35477151+nicodes@users.noreply.github.com>
2023-04-30 00:56:13 -0600

    Add NODE_ADAPTER env var (#14)
    * add NODE_ADAPTER env var
    
    * try with vercelStatic adapter
    
    * try edge

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,15 +1,15 @@
 import { defineConfig } from "astro/config";
 import node from "@astrojs/node";
-import vercel from "@astrojs/vercel/serverless";
+import vercelEdge from "@astrojs/vercel/edge";
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: !!process.env.VERCEL
-    ? vercel()
-    : node({
+  adapter: !!process.env.NODE_ADAPTER
+    ? node({
         mode: "standalone",
-      }),
+      })
+    : vercelEdge(),
   integrations: [svelte()],
 });
