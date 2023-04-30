e978d08b11d7dad7eaf356765148ce8ca7aca94b
nicodes <nicozessoules@gmail.com>
2023-04-29 23:59:51 -0600

    try with only vercel adapter

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -6,10 +6,6 @@ import svelte from "@astrojs/svelte";
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: process.env.NODE_ADAPTER
-    ? node({
-        mode: "standalone",
-      })
-    : vercelServerless(),
+  adapter: vercelServerless(),
   integrations: [svelte()],
 });
