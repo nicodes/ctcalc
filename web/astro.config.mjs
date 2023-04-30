0ed8509584fcc74893df83f4b665e9871739ae71
nicodes <nicozessoules@gmail.com>
2023-04-29 19:19:43 -0600

    add svelte

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
-
 import deno from "@astrojs/deno";
+
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: deno()
+  adapter: deno(),
+  integrations: [svelte()]
 });
