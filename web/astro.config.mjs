eeaa7899c60d55029f2eb2d1913453e83f3397d5
nicodes <nicozessoules@gmail.com>
2023-04-30 00:44:32 -0600

    add NODE_ADAPTER env var

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -6,10 +6,10 @@ import svelte from "@astrojs/svelte";
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: !!process.env.VERCEL
-    ? vercel()
-    : node({
+  adapter: !!process.env.NODE_ADAPTER
+    ? node({
         mode: "standalone",
-      }),
+      })
+    : vercel(),
   integrations: [svelte()],
 });
