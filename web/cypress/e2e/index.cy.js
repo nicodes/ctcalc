d5d04c3c55e9a3be573f23d96f3bf8e17064375b
nicodes <nicozessoules@gmail.com>
2023-04-29 23:22:06 -0600

    update test

--- web/cypress/e2e/index.cy.js
+++ web/cypress/e2e/index.cy.js
@@ -1,9 +1,16 @@
+const timeout = 10000;
+
 describe("Calculation Test", () => {
   it("does a calculation", () => {
+    // wait for astro hydration
+    cy.intercept("/_astro/**").as("astro");
     const page = cy.visit("http://localhost:3000");
-    page.get("input").first().type(1);
+    cy.wait("@astro");
+
+    page.get("input").type(1);
     page.get("select").last().select(0);
     page.get("button").click();
-    page.get("[data-cy=result]", { timeout: 10000 }).should("be.visible");
+
+    page.get("[data-cy=result]", { timeout }).contains("4.039520645285086");
   });
 });
